396e494fdc53dad6aa3e55edf7a7fa7ba34a3dd0
varun satvara <varunsatvara1997@gmail.com>
2019-11-19 14:18:39 +0530

    update

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// initialize scrollspy
+$('body').scrollspy({
+    target: '.dotted-scrollspy'
+});
+
+// initialize lightbox
+$(function() {
+    $("#mdb-lightbox-ui").load("../mdb-addons/mdb-lightbox-ui.html");
+});
+
+$('#collapseEx a').click(function(e) {
+    $('#collapseEx').collapse('hide');
+});
+
+/* WOW.js init */
+new WOW().init();
+
+$('.navbar-collapse a').click(function() {
+    $(".navbar-collapse").collapse('hide');
+})
